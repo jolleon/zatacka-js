16606701d13ec8d050c1161b275e516e032edb6a
Jules Olleon <jolleon@gmail.com>
2013-06-23 15:49:54 -0700

    duplicating all code for new version closer to original zatacka

---
+++ zatacka/snake.js
@@ -0,0 +1,95 @@
+Snake = function(){
+    this.x = 50;
+    this.y = 50;
+    this.old_x = 50;
+    this.old_y = 50;
+    this.radius = 3;
+    this.diameter = this.radius * 2;
+    this.speed = 3;
+    this.direction = 0;
+    this.dead = false;
+    this.rotation_speed = Math.PI / 64;
+};
+
+Snake.prototype.draw = function(ctx){
+    if (this.dead){
+        ctx.strokeStyle = "#f00";
+    }
+    else {
+        ctx.strokeStyle = "#fff";
+    };
+    ctx.lineWidth = this.diameter;
+    ctx.lineCap = 'round'; // square, butt(default)
+    ctx.beginPath();
+    ctx.moveTo(this.old_x, this.old_y);
+    ctx.lineTo(this.x, this.y);
+    ctx.stroke();
+};
+
+is_in_circle = function(x, y, a, b, r){
+    return ((Math.pow(x - a, 2) +
+            Math.pow(y - b, 2))
+            < Math.pow(r, 2));
+}
+
+is_in_line = function(x, y, x1, y1, x2, y2){
+    if (x < Math.min(x1, x2)) return false;
+    if (x > Math.max(x1, x2)) return false;
+    if (y < Math.min(y1, y2)) return false;
+    if (y > Math.max(y1, y2)) return false;
+    // at this point (x, y) is in the box ((x1,y1), (x2,y2))
+
+    // case of a vertical line
+    if (Math.abs(x1 - x2) < 1){
+        return (Math.abs(x - (x1+x2)/2) < 1);
+    }
+
+    var a = (y2 - y1) / (x2 - x1);
+    var b = y1 - a * x1;
+    var diff = y - a * x - b;
+    return (Math.abs(diff) <= 1);
+}
+
+Snake.prototype.move = function(){
+    if (this.dead) return;
+
+    this.old_x = this.x;
+    this.old_y = this.y;
+    this.x = this.old_x + this.speed * Math.cos(this.direction);
+    this.y = this.old_y + this.speed * Math.sin(this.direction);
+
+    // test death
+    var check_radius = Math.ceil(this.radius + this.speed);
+    var check_size = 2 * check_radius;
+    var top_x = Math.round(this.old_x - check_radius),
+        top_y = Math.round(this.old_y - check_radius);
+    var pix = ctx.getImageData(
+            top_x, top_y,
+            check_size, check_size).data;
+
+    for(var i=0, n = pix.length / 4; i<n; i+=1){
+        if (pix[4*i+3] !== 0){
+            var x = top_x + (i % check_size) + 0.5;
+            // not sure why we need this +0.5 - seems like webkit draws the
+            // circles half a pixel off or something :/
+            var y = top_y + Math.floor(i/check_size) + 0.5;
+            if ((is_in_circle(x, y, this.x, this.y, this.radius + 1) ||
+                is_in_line(x, y, this.x, this.y, this.old_x, this.old_y)) &&
+                !is_in_circle(x, y, this.old_x, this.old_y, this.radius + 1)) {
+                this.dead = true;
+            };
+        }
+    }
+    if (this.dead){
+        snakes_alive--;
+        snake_died++;
+    }
+};
+
+Snake.prototype.turnRight = function(){
+    this.direction -= this.rotation_speed;
+};
+Snake.prototype.turnLeft = function(){
+    this.direction += this.rotation_speed;
+};
+
---
+++ zatacka/zatacka.js
@@ -0,0 +1,181 @@
+var Key = {
+  _pressed: {},
+
+  isDown: function(keyCode) {
+    return this._pressed[keyCode];
+  },
+
+  onKeydown: function(event) {
+    konami.update(event.keyCode);
+    this._pressed[event.keyCode] = true;
+  },
+
+  onKeyup: function(event) {
+    delete this._pressed[event.keyCode];
+  }
+};
+
+var Konami = function(){
+    this.keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    this.state = 0;
+    this.update = function(keycode){
+        if (keycode === this.keys[this.state]){
+            this.state++;
+        }
+        else {
+            this.state = 0;
+        }
+        if (this.state === this.keys.length){
+            this.state = 0;
+            console.log("KONAMI");
+        }
+    }
+}
+
+konami = new Konami();
+
+
+var Keys = {
+    players: [
+        {
+            LEFT: 81, // Q
+            RIGHT: 87 // W
+        },
+        {
+            LEFT: 37, // left
+            RIGHT: 39, // right
+        },
+        {
+            LEFT: 86, // v
+            RIGHT: 66, // b
+        },
+        {
+            LEFT: 221, // ]
+            RIGHT: 220, // \
+        }
+    ],
+
+    SPACE: 32
+}
+
+Player = function(){
+    this.score = 0;
+}
+
+var Config = {
+    attrs: {
+        players: 2,
+        speed: 3,
+        size: 3,
+        rotation: 3
+    },
+
+    readConfig: function(){
+        for (attr in this.attrs){
+            this.attrs[attr] = parseInt($('input[name='+attr+']:checked').val());
+        }
+    },
+
+    updateConfigControls: function(){
+        for (attr in this.attrs){
+            $('input[name='+attr+'][value=' + this.attrs[attr] + ']').prop('checked', true);
+        }
+        for (var i=0; i<Keys.players.length; i++){
+            $('#left'+(i+1)).html(keyCodeToString[Keys.players[i].LEFT]);
+            $('#right'+(i+1)).html(keyCodeToString[Keys.players[i].RIGHT]);
+        }
+    },
+
+    get: function(attr){
+        if (attr === 'rotation'){
+            return this.get('speed') * this.attrs.rotation * Math.PI / 512;
+        }
+        else if (attr === 'speed'){
+            var s = this.attrs.speed + 2;
+            return s * s / 9;
+        }
+        else {
+            return this.attrs[attr];
+        }
+    }
+}
+
+var preparePlayers = function(){
+    players = [];
+    for (var i=0; i<Config.get('players'); i++){
+        players.push(new Player());
+    }
+}
+
+var prepareSnakes = function() {
+    snakes = [];
+    for(var i=0; i<players.length; i++){
+        snakes.push(new Snake());
+        snakes[i].radius = Config.get('size');
+        snakes[i].diameter = snakes[i].radius * 2;
+        snakes[i].speed = Config.get('speed');
+        snakes[i].rotation_speed = Config.get('rotation');
+    }
+    snakes_alive = snakes.length;
+
+    snakes[1].x = canvas.width - 50;
+    snakes[1].y = canvas.height - 50;
+    snakes[1].direction = Math.PI;
+
+    if (players.length > 2){
+        snakes[2].x = canvas.width - 50;
+        snakes[2].direction = Math.PI;
+    }
+
+    if (players.length > 3){
+        snakes[3].y = canvas.height - 50;
+    }
+}
+
+var updateGame = function(){
+    for (var i=0; i<players.length; i++){
+        if (Key.isDown(Keys.players[i].LEFT)) snakes[i].turnRight();
+        if (Key.isDown(Keys.players[i].RIGHT)) snakes[i].turnLeft();
+    }
+
+    if (snake_died > 0){
+        for (var i=0; i<snakes.length; i++){
+            if (!snakes[i].dead){
+                players[i].score += snake_died;
+            }
+        }
+        snake_died = 0;
+    }
+
+    if (Key.isDown(Keys.SPACE)){
+        if (snakes_alive < 2){
+            startGame();
+        }
+    }
+
+    if (snakes_alive > 1){
+        for (var i=0; i<snakes.length; i++){
+            var snake = snakes[i];
+            snake.move();
+        };
+    }
+
+};
+
+var drawGame = function(){
+    if(snakes){
+        for (var i=0; i<snakes.length; i++){
+            snakes[i].draw(ctx)
+        };
+    }
+
+    for (var i=0; i<4; i++){
+        var text = i < players.length ? players[i].score : '';
+        $("#score"+(i+1)).html(text);
+    }
+};
+
+var mainloop = function() {
+    updateGame();
+    drawGame();
+};
